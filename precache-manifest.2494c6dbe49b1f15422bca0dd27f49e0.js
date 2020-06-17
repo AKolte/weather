05378f2cdc69a2128714dc7c16d6942fbe96aac0
AKolte <adi.kolte@gmail.com>
2020-06-17 16:45:51 +0530

    Updates

---
+++ precache-manifest.2494c6dbe49b1f15422bca0dd27f49e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa98c9e59219f2cc8a44ed14f61597c",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ed1f423e3f7c56d3a095",
+    "url": "/weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4844c48db30579f6c473",
+    "url": "/weather/static/js/2.9968aa1e.chunk.js"
+  },
+  {
+    "revision": "28f49bec5ac2dc8ce9a86de5d1b1939e",
+    "url": "/weather/static/js/2.9968aa1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1f423e3f7c56d3a095",
+    "url": "/weather/static/js/main.1d00c788.chunk.js"
+  },
+  {
+    "revision": "b83e05f3b1e38480a3ef",
+    "url": "/weather/static/js/runtime-main.d16f45af.js"
+  }
+]);
